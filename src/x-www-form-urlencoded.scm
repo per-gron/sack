64ca5ac495eb8d84526c50292da1d45c7ae91d02
Per Eckerdal <per.eckerdal@gmail.com>
2011-09-14 10:36:15 +0200

    Fixes on x-www-form-urlencoded.scm (don't remember what, sorry)

--- src/x-www-form-urlencoded.scm
+++ src/x-www-form-urlencoded.scm
@@ -94,112 +94,111 @@
         (lambda ()
           (write-x-www-form-urlencoded fields)))))
 
-(define decode-x-www-form-urlencoded
-  (lambda (str)
-    (let ((n (string-length str)))
+(define (decode-x-www-form-urlencoded str)
+  (let ((n (string-length str)))
 
-      (define extract
-        (lambda (start len)
-          (let ((s (make-string len)))
-            (let loop ((i start) (j 0))
-              (if (< j len)
-                  (let ((c (string-ref str i)))
-                    (cond ((char=? c #\%)
-                           (cond ((hex (+ i 1))
-                                  =>
-                                  (lambda (x)
-                                    (string-set! s j (integer->char x))
-                                    (loop (+ i 3) (+ j 1))))
-                                 (else
-                                  #f)))
-                          ((char=? c #\+)
-                           (string-set! s j #\space)
-                           (loop (+ i 1) (+ j 1)))
-                          (else
-                           (string-set! s j c)
-                           (loop (+ i 1) (+ j 1)))))
-                  s)))))
+    (define extract
+      (lambda (start len)
+        (let ((s (make-string len)))
+          (let loop ((i start) (j 0))
+            (if (< j len)
+                (let ((c (string-ref str i)))
+                  (cond ((char=? c #\%)
+                         (cond ((hex (+ i 1))
+                                =>
+                                (lambda (x)
+                                  (string-set! s j (integer->char x))
+                                  (loop (+ i 3) (+ j 1))))
+                               (else
+                                #f)))
+                        ((char=? c #\+)
+                         (string-set! s j #\space)
+                         (loop (+ i 1) (+ j 1)))
+                        (else
+                         (string-set! s j c)
+                         (loop (+ i 1) (+ j 1)))))
+                s)))))
 
-      (define hex
-        (lambda (i)
-          (if (< (+ i 1) n)
-              (let ((h1 (nibble i))
-                    (h2 (nibble (+ i 1))))
-                (and h1 h2 (+ (* h1 16) h2)))
-              #f)))
+    (define hex
+      (lambda (i)
+        (if (< (+ i 1) n)
+            (let ((h1 (nibble i))
+                  (h2 (nibble (+ i 1))))
+              (and h1 h2 (+ (* h1 16) h2)))
+            #f)))
 
-      (define nibble
-        (lambda (i)
-          (let ((c (string-ref str i)))
-            (cond ((and (char>=? c #\0) (char<=? c #\9))
-                   (- (char->integer c) (char->integer #\0)))
-                  ((and (char>=? c #\a) (char<=? c #\f))
-                   (+ 10 (- (char->integer c) (char->integer #\a))))
-                  ((and (char>=? c #\A) (char<=? c #\F))
-                   (+ 10 (- (char->integer c) (char->integer #\A))))
-                  (else
-                   #f)))))
+    (define nibble
+      (lambda (i)
+        (let ((c (string-ref str i)))
+          (cond ((and (char>=? c #\0) (char<=? c #\9))
+                 (- (char->integer c) (char->integer #\0)))
+                ((and (char>=? c #\a) (char<=? c #\f))
+                 (+ 10 (- (char->integer c) (char->integer #\a))))
+                ((and (char>=? c #\A) (char<=? c #\F))
+                 (+ 10 (- (char->integer c) (char->integer #\A))))
+                (else
+                 #f)))))
 
-      (define state0 ; at beginning of string
-        (lambda (i rev-fields)
-          (if (< i n)
-              (state1 i
-                      i
-                      0
-                      rev-fields)
-              (reverse rev-fields))))
+    (define state0 ; at beginning of string
+      (lambda (i rev-fields)
+        (if (< i n)
+            (state1 i
+                    i
+                    0
+                    rev-fields)
+            (reverse rev-fields))))
 
-      (define state1 ; in field name
-        (lambda (i start len rev-fields)
-          (if (< i n)
-              (let ((c (string-ref str i)))
-                (cond ((char=? c #\=)
-                       (state2 (+ i 1)
-                               (+ i 1)
-                               0
-                               (extract start len)
-                               rev-fields))
-                      ((char=? c #\%)
-                       (and (hex (+ i 1))
-                            (state1 (+ i 3)
-                                    start
-                                    (+ len 1)
-                                    rev-fields)))
-                      (else
-                       (state1 (+ i 1)
-                               start
-                               (+ len 1)
-                               rev-fields))))
-              #f)))
+    (define state1 ; in field name
+      (lambda (i start len rev-fields)
+        (if (< i n)
+            (let ((c (string-ref str i)))
+              (cond ((char=? c #\=)
+                     (state2 (+ i 1)
+                             (+ i 1)
+                             0
+                             (extract start len)
+                             rev-fields))
+                    ((char=? c #\%)
+                     (and (hex (+ i 1))
+                          (state1 (+ i 3)
+                                  start
+                                  (+ len 1)
+                                  rev-fields)))
+                    (else
+                     (state1 (+ i 1)
+                             start
+                             (+ len 1)
+                             rev-fields))))
+            #f)))
 
-      (define state2 ; in field value
-        (lambda (i start len name rev-fields)
+    (define state2 ; in field value
+      (lambda (i start len name rev-fields)
 
-          (define end-of-field
-            (lambda ()
-              (cons (cons name (extract start len))
-                    rev-fields)))
+        (define end-of-field
+          (lambda ()
+            (cons (cons name (extract start len))
+                  rev-fields)))
 
-          (if (< i n)
-              (let ((c (string-ref str i)))
-                (cond ((char=? c #\&)
-                       (state1 (+ i 1)
-                               (+ i 1)
-                               0
-                               (end-of-field)))
-                      ((char=? c #\%)
-                       (and (hex (+ i 1))
-                            (state2 (+ i 3)
-                                    start
-                                    (+ len 1)
-                                    name
-                                    rev-fields)))
-                      (else
-                       (state2 (+ i 1)
-                               start
-                               (+ len 1)
-                               name
-                               rev-fields))))
-              (reverse (end-of-field)))))
+        (if (< i n)
+            (let ((c (string-ref str i)))
+              (cond ((char=? c #\&)
+                     (state1 (+ i 1)
+                             (+ i 1)
+                             0
+                             (end-of-field)))
+                    ((char=? c #\%)
+                     (and (hex (+ i 1))
+                          (state2 (+ i 3)
+                                  start
+                                  (+ len 1)
+                                  name
+                                  rev-fields)))
+                    (else
+                     (state2 (+ i 1)
+                             start
+                             (+ len 1)
+                             name
+                             rev-fields))))
+            (reverse (end-of-field)))))
 
-      (state0 0 '()))))
+    (state0 0 '())))
